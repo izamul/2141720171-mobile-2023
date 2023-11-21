c408c03d7869f936e432b26ff0fe2f9f7cdecb8c
izamulfikri <mfikri765@gmail.com>
2023-11-22 02:59:24 +0700

    W13: Jawaban Soal 6

--- week-13/src/stream_fikri/lib/main.dart
+++ week-13/src/stream_fikri/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stream_fikri/stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +32,9 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
 
   void changeColor() async {
     // await for (var eventColor in colorStream.getColors()) {
@@ -46,10 +51,28 @@ class _StreamHomePageState extends State<StreamHomePage> {
   }
 
   @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+  }
+
+  @override
   void initState() {
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
     super.initState();
-    colorStream = ColorStream();
-    changeColor();
   }
 
   @override
@@ -58,8 +81,19 @@ class _StreamHomePageState extends State<StreamHomePage> {
         appBar: AppBar(
           title: const Text('Stream Fikri'),
         ),
-        body: Container(
-          decoration: BoxDecoration(color: bgColor),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(lastNumber.toString()),
+              ElevatedButton(
+                onPressed: () => addRandomNumber(),
+                child: const Text('New Random Number'),
+              )
+            ],
+          ),
         ));
   }
 }
--- week-13/src/stream_fikri/lib/stream.dart
+++ week-13/src/stream_fikri/lib/stream.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 
 class ColorStream {
   final List<Color> colors = [
@@ -21,4 +21,16 @@ class ColorStream {
       return colors[index];
     });
   }
+}
+
+class NumberStream {
+  final StreamController<int> controller = StreamController<int>();
+
+  void addNumberToSink(int newNumber) {
+    controller.sink.add(newNumber);
+  }
+  
+  close() {
+    controller.close();
+}
 }
